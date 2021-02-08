9ee36a3a486441908031db457ecc08c25452a3ac
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2021-02-07 21:58:11 -0700

    practice.js

--- practice.js
+++ practice.js
@@ -1,6 +1,12 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
+class Vue {
+    constructor(options) {
+      const el = document.querySelector(options.el);
+      const data = options.data;
+      
+      Object.keys(data).forEach(key => {
+        el.innerHTML = el.innerHTML.replace(
+          `{{ ${key} }}`,
+          data[key]
+        );
+      });
+  }
