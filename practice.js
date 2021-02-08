b24b0f92a775368496196f920001c36c00ac0574
Michael Andrew Moore <74065788+MikeyAMoore@users.noreply.github.com>
2021-02-07 21:59:30 -0700

    practice.js

--- practice.js
+++ practice.js
@@ -15,13 +15,40 @@ class Vue {
         }
   
         if (n.nodeType === Node.TEXT_NODE) {
-          Object.keys(this.data).forEach(key => {
-            n.textContent = n.textContent.replace(
-              new RegExp(`{{ ${key} }}`, "g"),
-              this.data[key]
-            );
-          });
+          this.replaceText(n);
         }
       }
+    }
+  
+    replaceText(node) {
+      let text = node.textContent;
+      let result = "";
+  
+      let state = 0; // 0 searching template, 1 searching key
+      let cursor = 0;
+  
+      for (let i = 0; i < text.length - 1; i++) {
+        switch (state) {
+          case 0:
+            if (text[i] === "{" && text[i + 1] === "{") {
+              state = 1;
+              result += text.substring(cursor, i);
+              cursor = i;
+            }
+            break;
+          case 1:
+            if (text[i] === "}" && text[i + 1] === "}") {
+              state = 0;
+              result += this.data[text.substring(cursor + 2, i - 1).trim()];
+              cursor = i + 2;
+            }
+            break;
+          default:
+        }
+      }
+  
+      result += text.substring(cursor);
+  
+      node.textContent = result;
     }
   }
